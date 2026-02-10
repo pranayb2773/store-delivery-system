47259669e42365f16e34156b9e3f4af2e2556e77
Pranay Baddam <pranayb2773@gmail.com>
2026-02-10 16:48:04 +0000

    Install and configure Laravel Sanctum for API authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function (): void {
+            Route::middleware('api')
+                ->prefix('api/v1')
+                ->as('api.v1.')
+                ->group(base_path('routes/api.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
